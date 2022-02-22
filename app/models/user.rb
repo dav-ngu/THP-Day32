289cc2f519c2a38485b7446e4ec4d1ff25624751
davnguyen <dav-ngu31@hotmail.fr>
2022-02-22 15:22:44 +0100

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    has_many :organized_events, foreign_key: 'admin_id', class_name: "Event", dependent: :destroy
+    
+    has_many :attendances, foreign_key: 'participant_id', dependent: :destroy
+    has_many :events, through: :attendances
+
+    after_create :welcome_send
+
+    def welcome_send
+      UserMailer.welcome_email(self).deliver_now
+    end
+
+end
